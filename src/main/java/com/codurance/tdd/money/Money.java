a0670614be5a1617f8e3afe0da20b1a3efe7a8bd
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-10-01 16:13:01 +0100

    Refactor: use a factory method in Money (superclass) to remove direct references to Dollar subclass

--- src/main/java/com/codurance/tdd/money/Money.java
+++ src/main/java/com/codurance/tdd/money/Money.java
@@ -7,6 +7,15 @@ class Money {
 
     int amount;
 
+    /**
+     * Factory method to reduce direct references to subclasses
+     * @param amount amount
+     * @return a Dollar instance with the specified amount
+     */
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     @Override
     public boolean equals(Object object) {
         Money money = (Money) object;
--- src/test/java/com/codurance/tdd/money/DollarShould.java
+++ src/test/java/com/codurance/tdd/money/DollarShould.java
@@ -18,31 +18,31 @@ class DollarShould {
     @Test
     @DisplayName("multiply an amount by a number and receive an amount")
     void multiply_an_amount_by_a_number_and_receive_an_amount() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
+        Dollar five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
     }
 
     @Test
     @DisplayName("not have any side effects")
     void not_have_any_side_effects() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(15), five.times(3));
-        assertEquals(new Dollar(5), five);
+        Dollar five = Money.dollar(5);
+        assertEquals(Money.dollar(15), five.times(3));
+        assertEquals(Money.dollar(5), five);
     }
 
     @Test
     @DisplayName("make sure same amounts are equal")
     void make_sure_same_amounts_are_equal() {
-        Dollar five = new Dollar(5);
-        Dollar anotherFive = new Dollar(5);
+        Dollar five = Money.dollar(5);
+        Dollar anotherFive = Money.dollar(5);
         assertThat(five.equals(anotherFive), is(true));
     }
 
     @Test
     @DisplayName("make sure different amounts are not equal")
     void make_sure_different_amounts_are_not_equal() {
-        Dollar five = new Dollar(5);
-        Dollar six = new Dollar(6);
+        Dollar five = Money.dollar(5);
+        Dollar six = Money.dollar(6);
         assertThat(five.equals(six), is(false));
     }
 
@@ -57,6 +57,6 @@ class DollarShould {
     @Test
     @DisplayName("make sure same amounts in different currencies are not equal")
     void make_sure_same_amounts_in_different_currencies_are_not_equal() {
-        assertNotEquals(new Dollar(5), new Franc(5));
+        assertNotEquals(Money.dollar(5), new Franc(5));
     }
 }
