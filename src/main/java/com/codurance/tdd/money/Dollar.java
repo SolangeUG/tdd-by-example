99b19b98de88c862cb67fd644156dfb202c52156
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-10-01 15:02:15 +0100

    Translate a design objection (side effects) into a test case

--- src/main/java/com/codurance/tdd/money/Dollar.java
+++ src/main/java/com/codurance/tdd/money/Dollar.java
@@ -10,7 +10,7 @@ class Dollar {
         this.amount = amount;
     }
 
-    void times(int multiplier) {
-        amount *= multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
--- src/test/java/com/codurance/tdd/money/DollarShould.java
+++ src/test/java/com/codurance/tdd/money/DollarShould.java
@@ -16,7 +16,16 @@ class DollarShould {
     @DisplayName("multiply an amount by a number and receive an amount")
     void multiply_an_amount_by_a_number_and_receive_an_amount() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertThat(five.amount, is(10));
+        Dollar product = five.times(2);
+        assertThat(product.amount, is(10));
+    }
+
+    @Test
+    @DisplayName("not have any side effects")
+    void not_have_any_side_effects() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(3);
+        assertThat(product.amount, is(15));
+        assertThat(five.amount, is(5));
     }
 }
